450065bb32508d740d1ac275108f3b38b75901aa
liuyn <2212770755@qq.com>
2024-05-05 18:49:54 +0800

    update: 打包修改

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import { resolve } from "path";
+/** 路径查找 */
+const pathResolve = (dir) => {
+  return resolve(__dirname, ".", dir);
+};
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
@@ -10,6 +14,22 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    sourcemap: false,
+    // 消除打包大小超过500kb警告
+    chunkSizeWarningLimit: 4000,
+    rollupOptions: {
+      input: {
+        index: pathResolve("index.html"),
+      },
+      // 静态资源分类打包
+      output: {
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+        assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+      },
     },
   },
 });
